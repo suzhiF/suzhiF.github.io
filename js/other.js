00a33ebae4fcfb63335ffe3025fc5ca6b3562d09
suzhifa <13963543525@163.com>
2021-11-24 10:57:17 +0800

    Site updated: 2021-11-24 10:57:13)

---
+++ js/other.js
@@ -0,0 +1,21 @@
+$(function(){
+    $(window).scroll(function(){  //只要窗口滚动,就触发下面代码
+        var scrollt = document.documentElement.scrollTop + document.body.scrollTop; //获取滚动后的高度
+        if( scrollt >200 ){  //判断滚动后高度超过200px,就显示
+            $("#back_top").fadeIn(400); //淡入
+        }else{
+            $("#back_top").stop().fadeOut(400); //如果返回或者没有超过,就淡出.必须加上stop()停止之前动画,否则会出现闪动
+        }
+        /*文章目录页面固定*/
+        if (scrollt > 400) {
+            $('#catalog').css('position', 'fixed');
+            $('#catalog').css('top', '40px');
+        } else {
+            $('#catalog').css('position', 'absolute');
+            $('#catalog').css('top', '');
+        }
+    });
+    $("#back_top").click(function(){ //当点击标签的时候,使用animate在200毫秒的时间内,滚到顶部
+        $("html,body").animate({scrollTop:"0px"},200);
+    });
+});
